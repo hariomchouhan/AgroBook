8b0a4929144b96befc23c1f004b43e812340be4e
Hariom <hariomchouhan430@gmail.com>
2025-04-23 11:58:02 +0530

    Refactor Person Details Screen

--- app/(screens)/personDetails.tsx
+++ app/(screens)/personDetails.tsx
@@ -1,17 +1,15 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Header from "@/components/Header";
 import BackButton from "@/components/BackButton";
 import { colors } from "@/constants/theme";
 import Typo from "@/components/Typo";
-import { useLocalSearchParams } from "expo-router";
+import PersonDetailsShow from "@/components/PersonDetailsShow";
 
 type Props = {};
 
 const PersonDetails = (props: Props) => {
-  const { name, totalAmount, remainingAmount, paidAmount } =
-    useLocalSearchParams();
   return (
     <ScreenWrapper>
       <View style={styles.container}>
@@ -20,47 +18,11 @@ const PersonDetails = (props: Props) => {
           leftIcon={<BackButton />}
           style={{ marginBottom: 10 }}
         />
-        <View style={styles.personDetailsContainer}>
-          <View style={styles.personDetailsNameContainer}>
-            <Typo size={22} fontWeight={"800"} color={colors.primary}>
-              {name}
-            </Typo>
-          </View>
-          <View style={styles.personDetailsAmountContainer}>
-            <View style={styles.personDetailsBodyItem}>
-              <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
-                Total :{" "}
-                <Typo size={18} fontWeight={"600"} color={colors.neutral800}>
-                  {Number(totalAmount)}
-                </Typo>
-              </Typo>
-            </View>
-            <View style={styles.personDetailsBodyItem}>
-              <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
-                Paid :{" "}
-                <Typo size={18} fontWeight={"600"} color={colors.neutral800}>
-                  {Number(paidAmount)}
-                </Typo>
-              </Typo>
-            </View>
-            <View style={styles.personDetailsBodyItem}>
-              <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
-                Remaining :{" "}
-                <Typo
-                  size={18}
-                  fontWeight={"600"}
-                  color={
-                    Number(remainingAmount) === 0
-                      ? colors.neutral800
-                      : colors.error
-                  }
-                >
-                  {Number(remainingAmount)}
-                </Typo>
-              </Typo>
-            </View>
-          </View>
-        </View>
+
+        {/* Person Details */}
+        <PersonDetailsShow />
+
+        {/* Add Expense */}
         <ScrollView>
           <View>
             <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
@@ -78,32 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
     backgroundColor: colors.background,
-  },
-  personDetailsContainer: {
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderColor: colors.neutral200,
-    marginBottom: 20,
-  },
-  personDetailsNameContainer: {
-    justifyContent: "space-between",
-    alignItems: "center",
-    textAlign: "center",
-  },
-  personDetailsAmountContainer: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    gap: 20,
-    borderWidth: 1,
-    borderColor: colors.neutral200,
-    borderRadius: 10,
-    backgroundColor: colors.white,
-    marginVertical: 20,
-  },
-  personDetailsBodyItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
 });
 
---
+++ components/PersonDetailsShow.tsx
@@ -0,0 +1,84 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Typo from "./Typo";
+import { colors } from "@/constants/theme";
+import { useLocalSearchParams } from "expo-router";
+
+type Props = {};
+
+const PersonDetailsShow = (props: Props) => {
+  const { name, totalAmount, remainingAmount, paidAmount } =
+    useLocalSearchParams();
+  return (
+    <View style={styles.personDetailsContainer}>
+      <View style={styles.personDetailsNameContainer}>
+        <Typo size={22} fontWeight={"800"} color={colors.primary}>
+          {name}
+        </Typo>
+      </View>
+      <View style={styles.personDetailsAmountContainer}>
+        <View style={styles.personDetailsBodyItem}>
+          <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
+            Total :{" "}
+            <Typo size={18} fontWeight={"600"} color={colors.neutral800}>
+              {Number(totalAmount)}
+            </Typo>
+          </Typo>
+        </View>
+        <View style={styles.personDetailsBodyItem}>
+          <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
+            Paid :{" "}
+            <Typo size={18} fontWeight={"600"} color={colors.neutral800}>
+              {Number(paidAmount)}
+            </Typo>
+          </Typo>
+        </View>
+        <View style={styles.personDetailsBodyItem}>
+          <Typo size={18} fontWeight={"800"} color={colors.neutral800}>
+            Remaining :{" "}
+            <Typo
+              size={18}
+              fontWeight={"600"}
+              color={
+                Number(remainingAmount) === 0 ? colors.neutral800 : colors.error
+              }
+            >
+              {Number(remainingAmount)}
+            </Typo>
+          </Typo>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  personDetailsContainer: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderColor: colors.neutral200,
+    marginBottom: 20,
+  },
+  personDetailsNameContainer: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  personDetailsAmountContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    gap: 20,
+    borderWidth: 1,
+    borderColor: colors.neutral200,
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    marginVertical: 20,
+  },
+  personDetailsBodyItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export default PersonDetailsShow;
